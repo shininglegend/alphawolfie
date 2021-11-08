972e59ff77d4400406aeccc8c861b45ff12dcc52
shininglegend <72107680+shininglegend@users.noreply.github.com>
2021-11-08 15:55:06 -0600

    Update suggestion.py

--- Cogs/suggestion.py
+++ Cogs/suggestion.py
@@ -76,7 +76,7 @@ class Suggestion(commands.Cog):
         #add it to the database and return id
         for row in cur.execute('SELECT MAX(id) FROM suggestions2'):
             suggestionid = row[0]
-            suggestionid = 2222
+            #suggestionid = 2222
         suggestionid = int(suggestionid) + 1
         #post the suggestion
         embed2 = discord.Embed(color=Color.dark_magenta(), title=f"Suggestion #{suggestionid}", description=suggestion)
