03f80d327958d7daed442ef23cb3ceaaffdc84e1
shininglegend <72107680+shininglegend@users.noreply.github.com>
2021-11-08 15:54:38 -0600

    Update suggestion.py

--- Cogs/suggestion.py
+++ Cogs/suggestion.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 from discord import Embed, Color
 import sqlite3
 
-con = sqlite3.connect('data1.db')
+con = sqlite3.connect('/root/data/data1.db')
 cur = con.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS suggestions2 (id INTEGER PRIMARY KEY AUTOINCREMENT, msgid INTEGER, userid INTEGER, content TEXT, edited INTEGER DEFAULT 0, response TEXT DEFAULT 'None', responseid INTEGER DEFAULT 0)")
 #cur.execute('ALTER TABLE suggestions2 ADD responseid INTEGER DEFAULT 0')
